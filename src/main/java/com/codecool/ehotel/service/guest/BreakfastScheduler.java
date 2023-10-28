303ba085670f44ad6372b580a9ad9125bfb3c59a
AdrianMonemi <adrianmonemi6@gmail.com>
2023-10-28 15:44:30 +0200

    the tasks "prepare Breakfast Group"

--- src/main/java/com/codecool/ehotel/service/guest/BreakfastScheduler.java
+++ src/main/java/com/codecool/ehotel/service/guest/BreakfastScheduler.java
@@ -2,25 +2,23 @@ package com.codecool.ehotel.service.guest;
 
 import com.codecool.ehotel.model.Guest;
 
-import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.*;
 
 public class BreakfastScheduler {
-    public List<Set<Guest>> splitGuestsIntoBreakfastCycles(List<Guest> guests){
-        int numberOfCycles = 8;
 
+    public List<Set<Guest>> splitGuestsIntoBreakfastCycles(List<Guest> guests) {
+        int numberOfCycles = 8;
         List<Set<Guest>> breakfastCycles = new ArrayList<>(numberOfCycles);
-        for(int i = 0; i < numberOfCycles; i++){
+        for (int i = 0; i < numberOfCycles; i++) {
             breakfastCycles.add(new HashSet<>());
         }
 
         Random random = new Random();
-
         LocalTime startTime = LocalTime.of(6, 0);
-        LocalTime endTime = LocalTime.of(10,0);
+        LocalTime endTime = LocalTime.of(10, 0);
 
-        for(Guest guest : guests){
+        for (Guest guest : guests) {
             int cycleIndex = random.nextInt(numberOfCycles);
             LocalTime randomTime = startTime.plusMinutes((cycleIndex * 30));
 
@@ -28,6 +26,10 @@ public class BreakfastScheduler {
                 breakfastCycles.get(cycleIndex).add(guest);
             }
         }
+
         return breakfastCycles;
     }
 }
+
+
+
