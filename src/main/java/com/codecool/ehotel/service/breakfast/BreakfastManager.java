91278cc9363005758a97bfcd00f6e905a89061fd
Shcoobz <a.boeckmann23@gmail.com>
2023-10-28 16:58:01 +0200

    started implementing consume method in breakfastManager

--- src/main/java/com/codecool/ehotel/service/breakfast/BreakfastManager.java
+++ src/main/java/com/codecool/ehotel/service/breakfast/BreakfastManager.java
@@ -1,16 +1,17 @@
 package com.codecool.ehotel.service.breakfast;
 
+import com.codecool.ehotel.model.Guest;
 import com.codecool.ehotel.model.MealDurability;
 import com.codecool.ehotel.model.MealType;
 import com.codecool.ehotel.service.buffet.*;
+import com.codecool.ehotel.service.guest.GuestService;
+import com.codecool.ehotel.service.guest.GuestServiceImpl;
 import com.codecool.ehotel.ui.DisplayBreakfast;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class BreakfastManager {
 
@@ -32,6 +33,21 @@ public class BreakfastManager {
     LocalTime cycleStartTime = LocalTime.of(6, 0);  // start at 6:00 AM
     DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("h:mm a");  // to format the time to 6:00 AM
 
+    // Generate a list of random guests for the season
+    GuestService guestService = new GuestServiceImpl();
+    LocalDate seasonStart = LocalDate.now().withDayOfYear(1); // Start of the year
+    LocalDate seasonEnd = LocalDate.now().withDayOfYear(365); // End of the year
+    List<Guest> allGuests = new ArrayList<>();
+    int numberOfGuests = 100; // You can change this to the desired number of guests
+
+    for (int i = 0; i < numberOfGuests; i++) {
+      allGuests.add(guestService.generateRandomGuest(seasonStart, seasonEnd));
+    }
+
+    // Retrieve the guests for the current day
+    LocalDate today = LocalDate.now();
+    Set<Guest> guestsToday = guestService.getGuestsForDay(allGuests, today);
+
     for (int cycle = 1; cycle <= 8; cycle++) {
       System.out.println("\nStarting cycle: " + cycle + " - " + cycleStartTime.format(timeFormatter) + "\n");
 
@@ -42,7 +58,7 @@ public class BreakfastManager {
       displayBreakfast.showBreakfastMenu(buffetManager);
 
       // consume
-      // TODO: Implement consume logic here
+      consumeMeal(guestsToday);
 
       // discard
       // TODO: Implement discard logic here
@@ -74,6 +90,20 @@ public class BreakfastManager {
 
     // refill the buffet using the refillMap
     buffetService.refillBuffet(buffetManager, refillMap);
+  }
+
+  public void consumeMeal(Set<Guest> guests) {
+    for (Guest guest : guests) {
+      List<MealType> preferences = guest.guestType().getMealPreferences();
+      for (MealType preference : preferences) {
+        boolean consumed = buffetManager.consumeFreshest(preference);
+        if (consumed) {
+          System.out.println(guest.name() + " consumed " + preference);
+        } else {
+          System.out.println(guest.name() + " wanted " + preference + ", but it was unavailable.");
+        }
+      }
+    }
   }
 
  /* private static void testingConsumeMeal(BuffetManager buffetManager) {
--- src/main/java/com/codecool/ehotel/service/guest/GuestServiceImpl.java
+++ src/main/java/com/codecool/ehotel/service/guest/GuestServiceImpl.java
@@ -11,44 +11,86 @@ import java.util.Random;
 import java.util.Set;
 
 public class GuestServiceImpl implements GuestService {
-    @Override
-    public Guest generateRandomGuest(LocalDate seasonStart, LocalDate seasonEnd) {
+  private int guestIndex = 0; // keeps track of index of next guest to generate
+  private final Random random = new Random();
 
-        List<String> guestNames = List.of("Alice",
-                "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Henry",
-                "Isabella", "Jack", "Kate", "Liam", "Mia", "Noah",
-                "Olivia", "Paul", "Quinn", "Rachel", "Samuel", "Sophia",
-                "Thomas", "Ursula", "Victor", "Willow", "Xavier",
-                "Yvonne", "Zachary", "Abigail", "Benjamin", "Chloe");
+  @Override
+  public Guest generateRandomGuest(LocalDate seasonStart, LocalDate seasonEnd) {
 
-        Random random = new Random();
+/*    List<String> guestNames = List.of("Alice",
+        "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Henry",
+        "Isabella", "Jack", "Kate", "Liam", "Mia", "Noah",
+        "Olivia", "Paul", "Quinn", "Rachel", "Samuel", "Sophia",
+        "Thomas", "Ursula", "Victor", "Willow", "Xavier",
+        "Yvonne", "Zachary", "Abigail", "Benjamin", "Chloe");*/
 
-        String name = guestNames.get(random.nextInt(guestNames.size()));
+    List<String> guestNames = List.of(
+        "Alice", "Bob", "Charlie", "David", "Eva", "Frank", "Grace", "Henry",
+        "Isabella", "Jack", "Kate", "Liam", "Mia", "Noah",
+        "Olivia", "Paul", "Quinn", "Rachel", "Samuel", "Sophia",
+        "Thomas", "Ursula", "Victor", "Willow", "Xavier",
+        "Yvonne", "Zachary", "Abigail", "Benjamin", "Chloe",
+        "Daniel", "Emily", "Finn", "Gemma", "Harry", "Ivy",
+        "James", "Katherine", "Leo", "Megan", "Nathan", "Olivia",
+        "Peter", "Quincy", "Rose", "Sam", "Tessa", "Ulysses",
+        "Violet", "Walter", "Xena", "Yara", "Zane",
+        "Alex", "Bella", "Caleb", "Daisy", "Ethan", "Fiona",
+        "George", "Hannah", "Isaac", "Jessica", "Kai", "Lily",
+        "Mason", "Nora", "Oscar", "Penelope", "Quincy", "Ruby",
+        "Sofia", "Theodore", "Uma", "Vivian", "William", "Xander",
+        "Yasmine", "Zara", "Arthur", "Beatrice", "Charles", "Diana",
+        "Eleanor", "Freddie", "Giselle", "Henry", "Isabelle", "Jacob",
+        "Kylie", "Liam", "Mila", "Nolan", "Olive", "Patrick", "Quinn",
+        "Ronan", "Scarlett", "Tristan", "Ulysses", "Victoria", "Wesley",
+        "Xander", "Yasmine", "Zara", "Ava", "Benjamin", "Chloe",
+        "David", "Emma", "Finn", "Grace", "Hannah", "Isaac",
+        "Jacob", "Katherine", "Lily", "Mason", "Nora", "Oliver",
+        "Penelope", "Quincy", "Rose", "Sophia", "Thomas", "Ursula",
+        "Victor", "William", "Xander", "Yara", "Zane");
 
-        GuestType[] guestTypes = GuestType.values();
-        GuestType guestType = guestTypes[random.nextInt(guestTypes.length)];
 
-        LocalDate checkIn = seasonStart.plusDays(random.nextInt((int) ChronoUnit.DAYS.between(seasonStart, seasonEnd)));
-        LocalDate checkOut = checkIn.plusDays(random.nextInt(7));
-        if (checkOut.isAfter(seasonEnd)) {
-            checkOut = seasonEnd;
-        }
-        if(checkIn == checkOut){
-            checkOut = checkOut.plusDays(1);
-        }
+    // Random random = new Random();
 
+    List<GuestType> guestTypes = List.of(
+        GuestType.BUSINESS,
+        GuestType.TOURIST,
+        GuestType.KID
+    );
 
-        return new Guest(name, guestType, checkIn, checkOut);
+    if (guestIndex >= guestNames.size()) {
+      guestIndex = 0; // Reset the index if we've used all guest names
     }
 
-    @Override
-    public Set<Guest> getGuestsForDay(List<Guest> guests, LocalDate date) {
-        Set<Guest> guestsForToday = new HashSet<>();
-        for (Guest guest : guests) {
-            if (!date.isBefore(guest.checkIn()) && !date.isAfter(guest.checkOut())) {
-                guestsForToday.add(guest);
-            }
-        }
-        return guestsForToday;
+    String name = guestNames.get(random.nextInt(guestNames.size()));
+
+    // GuestType[] guestTypes = GuestType.values();
+    // GuestType guestType = guestTypes[random.nextInt(guestTypes.length)];
+
+    // use guestIndex to cycle through guest types
+    GuestType guestType = guestTypes.get(guestIndex % guestTypes.size());
+
+    guestIndex++; // increment the index for the next guest
+
+    LocalDate checkIn = seasonStart.plusDays(random.nextInt((int) ChronoUnit.DAYS.between(seasonStart, seasonEnd)));
+    LocalDate checkOut = checkIn.plusDays(random.nextInt(7));
+    if (checkOut.isAfter(seasonEnd)) {
+      checkOut = seasonEnd;
     }
+    if (checkIn == checkOut) {
+      checkOut = checkOut.plusDays(1);
+    }
+
+    return new Guest(name, guestType, checkIn, checkOut);
+  }
+
+  @Override
+  public Set<Guest> getGuestsForDay(List<Guest> guests, LocalDate date) {
+    Set<Guest> guestsForToday = new HashSet<>();
+    for (Guest guest : guests) {
+      if (!date.isBefore(guest.checkIn()) && !date.isAfter(guest.checkOut())) {
+        guestsForToday.add(guest);
+      }
+    }
+    return guestsForToday;
+  }
 }
