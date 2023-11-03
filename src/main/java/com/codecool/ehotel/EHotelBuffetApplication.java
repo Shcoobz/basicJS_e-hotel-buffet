0b613a7f9ef03de56d783324da21955c4072a51b
Shcoobz <a.boeckmann23@gmail.com>
2023-11-03 15:50:31 +0100

    removed some comments

--- src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
+++ src/main/java/com/codecool/ehotel/EHotelBuffetApplication.java
@@ -5,13 +5,9 @@ import com.codecool.ehotel.service.breakfast.BreakfastManager;
 // TODO: buffet menu
 //  - format list (names in center, dates & times right)
 //  - add count to print out?
-//  - make print out wider?
-//  - add titles to cycle steps? refill / consume / discard
-//  - add menu print out after each step?
 
 // TODO: general
 //  - make names pretty (Scrambled eggs instead of SCRAMBLED_EGGS)
-//  - move all souts to display
 
 public class EHotelBuffetApplication {
 
